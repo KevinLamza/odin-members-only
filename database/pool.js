4bd969ff529ee88587523f81a8ba4849937e8e60
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-03-09 20:01:08 +0300

    Add database query to add new user

--- database/pool.js
+++ database/pool.js
@@ -2,4 +2,8 @@ const { Pool } = require('pg');
 
 module.exports = new Pool({
     host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT,
 });
--- database/queries.js
+++ database/queries.js
@@ -1,1 +1,15 @@
-const { Pool } = require('pg');
+const pool = require('./pool.js');
+
+const insertUser = async (
+    firstName,
+    lastName,
+    email,
+    password,
+    isMember,
+    isAdmin,
+) => {
+    await pool.query(
+        'INSERT INTO users (first_name, last_name, email, password, is_member, is_admin) VALUES ($1, $2, $3, $4, $5, $6)',
+        [firstName, lastName, email, password, isMember, isAdmin],
+    );
+};
